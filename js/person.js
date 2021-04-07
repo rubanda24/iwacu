22580c57b96f997386f423f8e0b1bfb8690ec865
rubanda24 <79447474+rubanda24@users.noreply.github.com>
2021-04-07 15:36:19 +0200

    add setters and getters

--- js/person.js
+++ js/person.js
@@ -1,3 +1,79 @@
+class person {
+    constructor(pid,firstName,lastName,age,sex,phone,ubudehe){
+      this.pid = pid;
+     this.id = id,
+     this.firstName = firstName;
+     this.lastName = lastName;
+     this.age = age;
+     this.sex = sex;
+     this.phone = phone;
+     this.ubudehe = ubudehe;
+    }
+    // seters and geters of my fields
+    set id(id){
+      this.id = id;
+    }
+
+    get id(){
+      return this.id;
+    }
+    //first name
+    set firstName(firstName){
+      this.firstName = firstName;
+    }
+    get firstName(){
+      return this.firstName
+    }
+    //last name
+    set lastName(lastName){
+      this.lastName = lastName;
+    }
+    get lastName(){
+      return this.lastName
+    }
+    //age
+    set age(age){
+      this.age = age;
+    }
+    get age(){
+      return this.age;
+    }
+    //
+    name(){
+        return this.firstName +" "+ this.lastName+" "+ this.age;
+    }
+}
+
+
+const showName = () =>{
+    var person1 = new person(1,"siborurema","jean jaures",12,"male","0785624824",3);
+
+document.getElementById("p01").innerHTML = person1.name();
+
+}
+
+class visitor extends person{
+  constructor(pid,firstName,lastName,age,phone){
+    super(pid);
+
+    this.firstName=firstName;
+  }
+  fullName(){
+    return this.name()+" "+ this.firstName
+  }
+}
+
+
+const vis= () =>{
+  let v = new visitor("jimmy");
+  document.getElementById("p01").innerHTML = v.fullName();
+}
+
+
+
+
+
+
 class Car {
     constructor(name, year) {
       this.name = name;
